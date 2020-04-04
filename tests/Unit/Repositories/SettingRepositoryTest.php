c6948a9f3eff56ca22fda465adfe76d726bf6be4
Gabriel Anhaia <anhaia.gabriel@gmail.com>
2020-04-04 20:54:52 -0300

    feature: Implement unit test for setting repository 1. Getting a grid by name.

--- tests/Unit/Repositories/SettingRepositoryTest.php
+++ tests/Unit/Repositories/SettingRepositoryTest.php
@@ -21,7 +21,7 @@ class SettingRepositoryTest extends TestCase
      */
     public function testGetSettingWhenSettingNotFound()
     {
-        $settingName = 'test_setting_name';
+        $settingName = 'SETTING_NAME_TEST';
         $settingEloquentModelMock = \Mockery::mock(Setting::class);
 
         $settingEloquentModelMock->shouldReceive('where')
@@ -38,5 +38,44 @@ class SettingRepositoryTest extends TestCase
         $settingResponse = $settingRepository->getSetting($settingName);
 
         $this->assertNull($settingResponse);
+    }
+
+    /**
+     * Method responsible for test the method {@see SettingRepository::getSetting()} when the setting has been found.
+     */
+    public function testGetSetting()
+    {
+        $settingName = 'SETTING_NAME_TEST';
+        $settingValue = 'SETTING_VALUE_TEST';
+        $settingId = 9998988999889;
+
+        $settingEloquentModelMock = \Mockery::mock(Setting::class);
+
+        $expectedResponse = new \App\Entities\Setting($settingId);
+        $expectedResponse->setValue($settingValue)
+            ->setName($settingName);
+
+        $settingEloquentModelMock->shouldReceive('where')
+            ->once()
+            ->with('name', '=', $settingName)
+            ->andReturnSelf();
+
+        $settingEloquentModelMock->shouldReceive('first')
+            ->once()
+            ->withNoArgs()
+            ->andReturnSelf();
+
+        $settingEloquentModelMock->shouldReceive('getAttribute')
+            ->with('id')
+            ->andReturn($settingId);
+
+        $settingEloquentModelMock->shouldReceive('getAttribute')
+            ->with('value')
+            ->andReturn($settingValue);
+
+        $settingRepository = new SettingRepository($settingEloquentModelMock);
+        $settingResponse = $settingRepository->getSetting($settingName);
+
+        $this->assertEquals($expectedResponse, $settingResponse);
     }
 }
